95ceee3c06022f299114349efb2205d21ca199ad
soeun <121291640+soeuns@users.noreply.github.com>
2023-04-02 12:49:20 +0900

    Update Weather Observation Station 3.sql

--- Weather Observation Station 3.sql
+++ Weather Observation Station 3.sql
@@ -1,4 +1,4 @@
-https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 -- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
 
 SELECT DISTINCT CITY -- 중복된 항목 제외
