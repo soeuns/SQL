8737a3ceaa01afbd0cf8a3d1342d8d773a344493
soeun <121291640+soeuns@users.noreply.github.com>
2023-07-26 11:03:50 +0900

    Update LIMIT.sql

--- EASY/LIMIT.sql
+++ EASY/LIMIT.sql
@@ -1,10 +1,7 @@
 -- Weather Observation Station 15
 -- https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true
 
--- STATION 테이블에서 LONG_W 값을 소수점 네 자리까지 반올림하여 선택
 SELECT ROUND(LONG_W, 4)
-
--- LAT_N 값이 137.2345보다 작은 레코드들을 필터링
 FROM STATION
 WHERE LAT_N < 137.2345
 
@@ -13,3 +10,7 @@ ORDER BY LAT_N DESC
 
 -- 결과에서 가장 큰 값을 하나만 반환하도록 제한
 LIMIT 1;
+
+/*LIMIT 숫자 : 결과에서 몇개를 출력할지
+ORDER BY 컬럼 DESC: 내림차순
+*/
