4b9940a02374a3a83ba5ec368ad8d2064c5c9828
soeun <121291640+soeuns@users.noreply.github.com>
2023-07-26 11:04:10 +0900

    Update LIMIT.sql

--- EASY/LIMIT.sql
+++ EASY/LIMIT.sql
@@ -4,11 +4,7 @@
 SELECT ROUND(LONG_W, 4)
 FROM STATION
 WHERE LAT_N < 137.2345
-
--- LAT_N 값을 내림차순으로 정렬하여 가장 큰 값이 위에 위치하도록 함
 ORDER BY LAT_N DESC
-
--- 결과에서 가장 큰 값을 하나만 반환하도록 제한
 LIMIT 1;
 
 /*LIMIT 숫자 : 결과에서 몇개를 출력할지
